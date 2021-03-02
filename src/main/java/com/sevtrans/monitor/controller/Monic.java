519100c75a44d341d3711bfbac97fa11568e5072
gem <subs_gem@mail.ru>
2021-03-02 08:56:18 +0300

    JasperReports

--- src/main/java/com/sevtrans/monitor/controller/Monic.java
+++ src/main/java/com/sevtrans/monitor/controller/Monic.java
@@ -3,9 +3,16 @@ package com.sevtrans.monitor.controller;
 import com.sevtrans.monitor.dto.Shell;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+import static org.springframework.http.MediaType.*;
 
 @Slf4j
 @CrossOrigin //TODO don't forget about
@@ -17,5 +24,21 @@ public class Monic {
     public ResponseEntity GetMessage(@RequestBody Shell shell) {
         System.out.println(shell.getMsgType());
         return ResponseEntity.ok(HttpStatus.OK);
+    }
+
+    @GetMapping(value = "/report", produces = APPLICATION_PDF_VALUE)
+    public @ResponseBody
+    byte[] getFile() throws IOException {
+        InputStream in = getClass()
+                .getResourceAsStream("/report/Employee_report.pdf");
+        ByteArrayOutputStream buffer = new ByteArrayOutputStream();
+
+        int nRead;
+        byte[] data = new byte[16384];
+
+        while ((nRead = in.read(data, 0, data.length)) != -1) {
+            buffer.write(data, 0, nRead);
+        }
+        return  buffer.toByteArray();//IOUtils.toByteArray(in);//in.readAllBytes();//
     }
 }
